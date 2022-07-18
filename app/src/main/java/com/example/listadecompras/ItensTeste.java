114861d6e750f9c738b775feb923744645298691
Gabriel <gfcorrea01@gmail.com>
2022-07-18 13:11:24 -0300

    Versão 1.0

---
+++ app/src/main/java/com/example/listadecompras/ItensTeste.java
@@ -0,0 +1,12 @@
+package com.example.listadecompras;
+
+public class ItensTeste {
+
+    String nome;
+    String desc;
+
+    public ItensTeste(String nome, String desc) {
+        this.nome = nome;
+        this.desc = desc;
+    }
+}
